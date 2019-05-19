8fba360ff14f929d81d420e7135cc972611df20d
antoniokht <antoniokingdom@hotmail.com>
2019-05-19 19:09:20 -0300

    Enviando segunda parte, com Franc, Money e testes

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,6 +1,12 @@
 
-public class Dollar {
-	int amount = 10;
-    Dollar(int amount) {}			
-    void times(int multiplier) {}
+public class Dollar extends Money{
+	
+   Dollar(int amount) {
+      this.amount= amount;
+   }
+
+   Dollar times(int multiplier) {
+      return new Dollar(this.amount * multiplier);
+   }
+   
 }
